4fce8c01bb4650fa733021150be08e2d508a745e
Fernando Caldera <fcaldera@cebroker.com>
2017-08-11 09:32:15 -0500

    Include react-router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './components';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { REACT_APP_URL_BASE = '/' } = process.env;
+
+ReactDOM.render(
+  <BrowserRouter basename={REACT_APP_URL_BASE}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();

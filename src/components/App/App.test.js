88e1f5f676adabcc5880a6c26a58a603a56d26e1
Santiago Mendoza <smendoza@isf.com.co>
2017-08-18 18:25:50 -0500

    Fix App tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,15 +4,15 @@ import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import App from './App';
 
+const user = { firstName: 'Jhon', lastName: 'Doe' };
+
 it('renders without crashing', () => {
-  shallow(<App />);
+  shallow(<App user/>);
 });
 
 it('renders correctly - snapshot', () => {
-  const tree = renderer.create(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  );
+  const tree = renderer.create(<MemoryRouter>
+      <App user/>
+    </MemoryRouter>);
   expect(tree).to.matchSnapshot();
 });

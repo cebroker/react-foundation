294d39dc659fa7a2d6c39c36da089b5655e1374b
Santiago Mendoza <smendoza@isf.com.co>
2017-08-18 15:47:16 -0500

    Refactor expect import from chai package

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,6 @@
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 import chaiJestSnapshot from 'chai-jest-snapshot';
-import { expect } from 'chai';
 chai.use(chaiEnzyme());
 chai.use(chaiJestSnapshot);
 global.expect = expect;

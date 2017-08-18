086344378c829b2774ef467b73e727acce430f0d
Santiago Mendoza <smendoza@isf.com.co>
2017-08-18 11:29:29 -0500

    Add snapshot testing to App Component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,6 +4,7 @@ import chai, { expect } from 'chai';
 import { shallow } from 'enzyme';
 import chaiEnzyme from 'chai-enzyme';
 import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 chai.use(chaiEnzyme());
@@ -11,4 +12,9 @@ chai.use(chaiEnzyme());
 it('renders without crasing', () => {
   const wrapper = shallow(<App />);
   expect(wrapper).to.be.present();
+});
+
+it('renders correctly - snapshot', () => {
+  const tree = shallow(<App />);
+  jestExpect(tree).toMatchSnapshot();
 });
---
+++ src/setupTests.js
@@ -0,0 +1,1 @@
+global.jestExpect = expect;
